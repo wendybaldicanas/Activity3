3c42f9444c37e2d08ea21520ee333c24c401685b
wendybaldicanas <wendy.baldicanas28@gmail.com>
2026-02-18 10:25:09 +0800

    Second Update

--- inventory.dart
+++ inventory.dart
@@ -1,11 +1,43 @@
-class Product {
-  String _name = "";
-  double price = 0;
-  int quantity = 0;
+import 'dart:io';
 
-  Product (String name, double price, int quantity) {
-    this. _name = name;
-    this.price = price;
-    this.quantity =  
+class Product {
+  String _name = 
+  double price = 
+  int quantity = 
+
+  Product (this.name, this.price, this.quantity); 
   }
-}
+
+  void main() {
+    List<Product> inventory = [];
+
+    while (true) {
+      print("\nInventory System");
+      print("1. Add Product");
+      print("2. View Products");
+      print("3. Sell Products");
+      print("4. Exit");
+
+      stdout.write("Enter your choice: ");
+      String? choice = stdin.readLineSync();
+
+      switch (choice) {
+        case '1':
+          addProduct(inventory);
+          break;
+        case '2':
+          viewProducts(inventory);
+          break;
+        case '3':
+          sellProduct(inventory);
+          break;
+        case '4':
+          print("Exiting program...");
+          return;
+        default;
+          print("Invalid choice. Try again."); 
+      }
+    }
+  }
+
+  void addProduct(List<Product> Inventory)

29fc7fbc958974140de48806b5ef50675caebdf8
wendybaldicanas <wendy.baldicanas28@gmail.com>
2026-02-18 10:10:37 +0800

    First Update

---
+++ inventory.dart
@@ -0,0 +1,11 @@
+class Product {
+  String _name = "";
+  double price = 0;
+  int quantity = 0;
+
+  Product (String name, double price, int quantity) {
+    this. _name = name;
+    this.price = price;
+    this.quantity =  
+  }
+}
